da5b2912996be07b8f284c08d7fe6be3b9b0cd5d
darvil82 <davidlosantos89@gmail.com>
2026-04-26 15:08:53 +0200

    layout tweaks

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,6 @@
 export const prerender = true;
 export const ssr = false;
+
+export const load = () => {
+	document.oncontextmenu = () => false;
+};
--- src/routes/menu/+page.ts
+++ src/routes/menu/+page.ts
@@ -2,6 +2,5 @@ import { Menu } from '$lib/scripts/menu/menu';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = () => {
-	document.oncontextmenu = () => false;
 	Menu._initialize();
 };
